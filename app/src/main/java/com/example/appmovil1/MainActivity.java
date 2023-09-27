4c458cf1137c473da60276093d7741900a722913
Sebastian <sebastian.nayar@hotmail.com>
2023-09-27 13:57:22 -0300

    Comentario

--- app/src/main/java/com/example/appmovil1/MainActivity.java
+++ app/src/main/java/com/example/appmovil1/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+/*Medio Pene*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
