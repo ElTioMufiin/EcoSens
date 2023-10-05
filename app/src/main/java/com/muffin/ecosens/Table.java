60a64576122bd0746738788940c378fcbde4e91e
ElTioMufiin <alvaro.toro.n@gmail.com>
2023-10-05 06:44:15 -0300

    Test

--- app/src/main/java/com/muffin/ecosens/Table.java
+++ app/src/main/java/com/muffin/ecosens/Table.java
@@ -2,16 +2,48 @@ package com.muffin.ecosens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Table extends AppCompatActivity {
 
+
+    public ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
+
+        DataController.cargarData();
+        AdaptadorData adapter = new AdaptadorData (this);
+        listView = findViewById(R.id.listData);
+        listView.setAdapter(adapter);
+
+
     }
 }
+class AdaptadorData extends ArrayAdapter<Data> {
+    final AppCompatActivity appCompatActivity;
+
+    public AdaptadorData(AppCompatActivity context) {
+        super(context, R.layout.data, DataController.getListado());
+        appCompatActivity = context;
+    }
+
+    public View getView(int i, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = appCompatActivity.getLayoutInflater();
+        View item = inflater.inflate(R.layout.data, null);
+
+        TextView tableTest = item.findViewById(R.id.listData);
+        tableTest.setText(DataController.getListado().get(i).getAirQ()+"%");
+
+        return(item);
+
+    }
+}
+
