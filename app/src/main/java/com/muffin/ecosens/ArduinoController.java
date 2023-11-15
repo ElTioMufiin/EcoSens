1a5164278f08ef763fce0a3799bf5e7e9765fa58
ElTioMufiin <alvaro.toro.n@gmail.com>
2023-11-15 01:51:22 -0300

    Push Y lectura FireBase Arduino

--- app/src/main/java/com/muffin/ecosens/ArduinoController.java
+++ app/src/main/java/com/muffin/ecosens/ArduinoController.java
@@ -5,25 +5,17 @@ import java.util.ArrayList;
 public class ArduinoController {
     private static ArrayList<Arduino> listaArduino = new ArrayList<>();
 
-    public static Arduino addArduino(int id, String ip, String estado, String nombre, String ubicacion){
+    public static Arduino addArduino(String id, String ip, String estado, String nombre, String ubicacion){
         Arduino a = new Arduino(id,ip,estado,nombre,ubicacion);
         listaArduino.add(a);
         return a;
     }
-    public static Arduino findArduino(int id){
-        for (Arduino a : listaArduino){
-            if(id==a.getId()){
-                return a;
-            }
-        }
-        return null;
-    }
 
     public static ArrayList<Arduino> getListaArduino(){return listaArduino;}
 
-    public static void cargarArrayArduino(){
-        if(listaArduino.size()==0){
-            addArduino(1,"192.168.69.50","Activo","Ventana Comedor","33.7°S 70.5°W");
-            addArduino(2,"192.168.69.51","Activo","Patio","40.7128° N, 74.0060° W");
-        }
-}}
+//    public static void cargarArrayArduino(){
+//        if(listaArduino.size()==0){
+//            addArduino(1,"192.168.69.50","Activo","Ventana Comedor","33.7°S 70.5°W");
+//            addArduino(2,"192.168.69.51","Activo","Patio","40.7128° N, 74.0060° W");
+//        }
+}
